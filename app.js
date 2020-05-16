9a480e12becee78239713b604efade9a1868e086
Dory Le <59546117+doryle@users.noreply.github.com>
2020-05-16 15:20:00 -0700

    Comment route set up

--- app.js
+++ app.js
@@ -1,17 +1,68 @@
 const   express     = require("express"),
-        app         = express();
+        app         = express(),
+        mongoose    = require("mongoose");
+        bodyParser  = require("body-parser"),
+        passport    = require("passport"),
+        cookieParser = require("cookie-parser"),
+        LocalStrategy = require("passport-local"),
+        flash        = require("connect-flash"),
+        // Comment     = mongoose.model(Comment, commentSchema),
+        // User        = require("./models/user"),
+        session = require("express-session"),
+        methodOverride = require("method-override");
 
-// app.set('view engine', 'ejs');
+
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost/blogdb', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Schema Setup
+var commentSchema = new mongoose.Schema({
+    author: String,
+    text: String,
+});
+
+var Comment = mongoose.model("Comment", commentSchema)
 
 
+//Routes
 app.get("/", function(req, res){
     res.render("index.ejs");
 });
 
+//Show all comments
 app.get("/blog", function(req, res){
-    res.render("blog.ejs")
+    Comment.find({}, function(err, allComments){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("blog.ejs", {comment:allComments})
+        }
+    });
 });
+
+//Add new Comment and save to DB
+app.post("/blog", function(req, res){
+    var text = req.body.text;
+    var author = req.body.author;
+    var newComment = {
+        author: author,
+        text: text }
+    Comment.create(newComment, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/blog");
+        }
+    });
+});
+
+//Form to create new comments
+app.get("/new", function(req, res){
+    res.render("new.ejs");
+});
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
